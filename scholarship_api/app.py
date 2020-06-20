f5d4dda7a27b0a0ed5076fe882a34969acb0d022
MrSwag <Nikhil Sharma>
2020-06-19 21:31:40 -0700

    initial version of chalice working

---
+++ scholarship_api/app.py
@@ -0,0 +1,138 @@
+from chalice import Chalice
+from chalicelib import parseUWTranscript
+import pymysql
+import sys
+import base64
+from configparser import ConfigParser  
+import PyPDF2 as ppy
+
+app = Chalice(app_name='scholarship_api')
+
+config = ConfigParser()  
+config.read('./chalicelib/config.ini')  
+
+# get rds creds from config
+REGION = config.get('rds', 'region')
+rds_host  = config.get('rds', 'host')
+name = config.get('rds', 'username')
+password = config.get('rds', 'password')
+db_name = config.get('rds', 'db_name')
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+@app.route('/createStudent', methods=['POST'])
+def create_student():
+    body = app.current_request.json_body
+    result = []
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    with conn.cursor() as cur:
+        cur.execute("""select * from tbl_students WHERE email = '%s'""" % (body['email']))
+        result = cur.fetchone()
+        if (result == None):
+            cur.execute("""insert into tbl_students (email, firstName, lastName) 
+            values( '%s', '%s', '%s')""" 
+            % (body['email'], body['firstName'], body['lastName']))
+            conn.commit()
+            cur.close()
+            return {
+                'statusCode' : 200,
+            }
+        else:
+            cur.close()
+            return {
+                'statusCode' : 400,
+                'error' : 'Student Already Exists'
+            }
+
+
+@app.route('/createGroup', methods=['POST'])
+def create_group():
+    body = app.current_request.json_body
+    result = []
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    with conn.cursor() as cur:
+        cur.execute("""insert into tbl_groups (name, description) 
+        values( '%s', '%s', '%s')""" 
+        % (body['name'], body['description']))
+        conn.commit()
+
+        cur.close()
+        return {
+            'statusCode' : 200
+        }
+    # not sure how with as statements work, my thought is that if the sql fails
+    # it will come here and if it succeeds it will return before this
+    return {
+        'statusCode' : 400
+    }
+
+
+@app.route('/joinGroup', methods=['POST'])
+def join_group():
+    body = app.current_request.json_body
+    groupID = body['groupID']
+    studentID = body['studentID']
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+    with conn.cursor() as cur:
+        cur.execute("""insert into tbl_groupsStud (studentID, groupID) 
+        values( '%s', '%s')""" 
+        % (body['studentID'], body['groupID']))
+        conn.commit()
+
+        cur.close()
+        return {
+            'statusCode' : 200
+        }
+    # not sure how with as statements work, my thought is that if the sql fails
+    # it will come here and if it succeeds it will return before this
+    return {
+        'statusCode' : 400,
+        'error' : 'Student Already Exists'
+    }
+
+
+@app.route('/uploadTranscript', methods=['POST'])
+def upload_transcript():
+    body = app.current_request.json_body
+    # get the base64 encoded pdf
+    pdf_base64 = body['transcriptData']
+    with open("transcript.pdf", "wb") as transcript:
+        # write the base64 to the file
+        base64_message = pdf_base64
+        base64_bytes = base64_message.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        transcript.write(message_bytes)
+        # parse the pdf
+        pdf2 = ppy.PdfFileReader("transcript.pdf")
+        classes = parseUWTranscript.getClasses(pdf2)
+        print(classes)
+        return {
+            'statusCode' : 200
+        }
+    return {
+        'statusCode' : 400,
+        'error' : 'Student Already Exists'
+    }
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ scholarship_api/chalicelib/parseUWTranscript.py
@@ -0,0 +1,109 @@
+# %%
+import PyPDF2 as ppy
+#from addClass import main as ac
+#from addStudent import main as ass
+#from addStudClass import main as asc
+import pandas as pd
+import pymysql
+import io
+
+# %%
+# pdf = ppy.PdfFileReader("./Transcript for Robi.pdf")
+# pdf2 = ppy.PdfFileReader("UWUnofficialTranscript.pdf")
+# text = textract.process("UWUnofficialTranscript.pdf")
+# otherPDF = text.decode("utf-8")
+
+
+# %%
+# data = getClasses(pdf2)
+# studentID = data['studentID']
+# data = data['data']
+# print(data)
+# print(studentID)
+
+# %%
+# def commitData(data, studentID):
+#     classes=data
+# #     studentID = data['studentID']
+# #     if (studentID == userID):     
+#     row = data.loc[0]
+#     classesBatch = []
+#     for i in range(len(data)):
+#         row = data.loc[i]
+#         classesBatch.append((row['Department'], row['ClassNum'], row['ClassTitle']))
+#     ac.main(classesBatch)
+    
+#     row = data.loc[0]
+#     studClassBatch = []
+#     for i in range(len(data)):
+#         row = data.loc[i]
+#         studClassBatch.append((studentID, row['Quarter'], row['ClassNum'], row['Department'], row['ClassTitle'], row['Year']))
+#     asc.main(studClassBatch)
+
+    
+
+# %%
+def mergePages(pages):
+    breaks = []
+    total = []
+    # Look for the word 'Page' on each page 
+    # assumptions: there is only one occurance
+    # no valuable data preceds the occurance of 'Page'
+    curQtr = ""
+    studentNumber = ""
+    for i in range(pages.getNumPages()):
+        page = pages.getPage(i)
+        parsed = page.extractText().split('\n')
+        studentNumber = parsed[8]
+        for i,x in enumerate(parsed):
+            if "Page" in x:
+                breaks = i
+            if 'CURRENTLY ENROLLED' in x:
+                curQtr = x.split('(')[1].split(')')[0].replace('QUARTER, ', "")        
+        useful = parsed[breaks + 1: ]
+        total.extend(useful)
+    return {'total': total, 'curQtr': curQtr.strip(), 'studentID': studentNumber}
+
+# %%
+def getClasses(pdf):
+    information = mergePages(pdf)
+    parsed = information['total']
+    curQtr = information['curQtr']
+    studentID = information['studentID']
+    durations = pd.DataFrame(columns = ['Quarter', 'Year', 'StatusWhile', 'index'])
+    count = 0
+    for i, x in enumerate(parsed):
+        if 'AUTUMN' in x or 'WINTER' in x or 'SPRING' in x and 'D E S T R O Y' not in x:
+            row = {'Quarter': x.split()[0], 'Year': x.split()[1], 'StatusWhile' : x.split()[2], 'index': i}
+    #         y = x.split()
+            durations = durations.append(row, ignore_index=True)
+            count = count +1
+
+    classes = pd.DataFrame(columns = ['Quarter', 'Department', 'ClassNum', 'ClassTitle', 'Grade'])
+    for i in durations.iloc:
+        ind = i['index'] + 1
+        cur = parsed[ind]
+        while 'QTR' not in cur and 'EARNED' not in cur and 'D E S T R O Y' not in cur and ind < len(parsed):
+            if 'GEN ST' in cur:
+                cur = cur.replace('GEN ST' ,"GEN")
+            curSplit = cur.split()
+            desc = ""
+            grade =  curSplit[-1]
+            for j in range(2, len(curSplit) - 2):
+                desc = desc + " " + curSplit[j]
+            if curQtr == str(i['Quarter'] + " " +i['Year']):
+                desc = ""
+                for j in range(2, len(curSplit) - 1):
+                    desc = desc + " " + curSplit[j]
+                grade = "IP"
+            row = {'Quarter': i['Quarter'].strip(), 'Year': i['Year'].strip(), 'Grade': grade.strip(), 'Department': curSplit[0].strip(), 'ClassNum': curSplit[1].strip(), 'ClassTitle' : desc.strip() }
+            classes = classes.append(row, ignore_index=True)
+            ind = ind + 1
+            cur = parsed[ind]
+            
+    return {'data': classes, 'studentID': studentID}
+
+# %%
+# commitData(data, studentID)
+
+# %%
